ad44a601e8a68646c78fcccddeaf4634aebab043
talya <talya.solomon1@mail.huji.ac.il>
2024-05-31 17:29:14 +0300

    last update for FRIDAY

--- uthreads.c
+++ uthreads.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <glib.h>
 #include <setjmp.h>
+#include <stdbool.h>
 
 #define READY 0
 #define RUNNING 1
@@ -12,11 +13,13 @@ typedef unsigned long address_t;
 #define JB_SP 6
 #define JB_PC 7
 
+int TOTAL_QUANTUMS = 0;
 
 typedef struct Thread {
   int tid;
   int state;
   int n_quantum;
+  int wake_up_time;
   thread_entry_point entry_point;
   char stack[STACK_SIZE];
   sigjmp_buf env;
@@ -38,9 +41,46 @@ address_t translate_address(address_t addr)
   return ret;
 }
 
+bool is_valid_tid(int tid)
+{
+  if (tid < 0 || tid >= MAX_THREAD_NUM || threads[tid] == NULL)
+  {
+    printf("thread library error: invalid tid\n");
+    return false;
+  }
+  return true;
+}
+
 int uthread_init(int quantum_usecs) {
   ready_queue = g_queue_new();
-  // TODO: Implement this function
+    if (ready_queue == NULL)
+    {
+        return -1;
+    }
+    for (int i = 0; i < MAX_THREAD_NUM; i++)
+    {
+        threads[i] = NULL;
+    }
+    Thread* main_thread = malloc(sizeof(Thread));
+    if (main_thread == NULL)
+    {
+        return -1;
+    }
+    main_thread->tid = 0;
+    main_thread->state = RUNNING;
+    main_thread->n_quantum = 1;
+    main_thread->entry_point = NULL;
+    sigsetjmp(main_thread->env, 1);
+//    address_t sp = (address_t) main_thread->stack + STACK_SIZE - sizeof
+//        (address_t);
+//    address_t pc = (address_t) main_thread->entry_point;
+//    (main_thread->env->__jmpbuf)[JB_SP] = translate_address(sp);
+//    (main_thread->env->__jmpbuf)[JB_PC] = translate_address(pc);
+//    sigemptyset(&main_thread->env->__saved_mask);
+    threads[0] = main_thread;
+    current_thread = 0;
+
+
   return 0;
 }
 
@@ -81,64 +121,114 @@ int uthread_spawn(thread_entry_point entry_point) {
 
 void running_next_thread()
 {
+  if (g_queue_is_empty(ready_queue))
+  {
+    printf("thread library error: no ready threads\n");
+    exit(1);
+  }
+
   Thread* next_thread = g_queue_pop_head(ready_queue);
   g_queue_push_tail(ready_queue, threads[current_thread]);
   current_thread = next_thread->tid;
   next_thread->state = RUNNING;
+  next_thread->n_quantum++;
   siglongjmp(next_thread->env, 1);
 }
 
-int uthread_terminate(int tid) {
-  if (tid < 0 || tid >= MAX_THREAD_NUM || threads[tid] == NULL)
+void switch_out_current_thread()
+{
+  if (sigsetjmp(threads[current_thread]->env, 1) == 0)
   {
-    printf("thread library error: invalid tid\n");
-    return -1;
+    running_next_thread();
   }
-    if (tid == 0)
-    {
-        exit(0);
-    }
-    if (threads[tid]->state == RUNNING)
-    {
-      running_next_thread();
-    }
+}
 
-    if (threads[tid]->state == READY)
-    {
-      g_queue_remove(ready_queue, threads[tid]);
-    }
+int uthread_terminate(int tid) {
+  if (!is_valid_tid(tid)) return -1;
+  if (tid == 0)
+  {
+      exit(0);
+  }
+  if (threads[tid]->state == RUNNING)
+  {
+    running_next_thread();
+  }
 
-    free(threads[tid]);
-    threads[tid] = NULL;
+  if (threads[tid]->state == READY)
+  {
+    g_queue_remove(ready_queue, threads[tid]);
+  }
+
+  free(threads[tid]);
+  threads[tid] = NULL;
   return 0;
 }
 
 int uthread_block(int tid) {
-  // TODO: Implement this function
+  if (!is_valid_tid(tid)) return -1;
+
+  if (tid == 0)
+  {
+      printf("thread library error: blocking main thread\n");
+      return -1;
+  }
+
+  if (threads[tid]->state == RUNNING)
+  {
+    running_next_thread();
+  }
+
+  if (threads[tid]->state == READY)
+  {
+    g_queue_remove(ready_queue, threads[tid]);
+  }
+
+  threads[tid]->state = BLOCKED;
+  sigsetjmp(threads[tid]->env, 1);
   return 0;
 }
 
 int uthread_resume(int tid) {
-  // TODO: Implement this function
+  if (!is_valid_tid(tid)) return -1;
+
+  threads[tid]->state = READY;
+  g_queue_push_tail(ready_queue, threads[tid]);
   return 0;
 }
 
 int uthread_sleep(int num_quantums) {
-  // TODO: Implement this function
+  if (current_thread == 0)
+  {
+      printf("thread library error: main thread called sleep\n");
+      return -1;
+  }
+
+  Thread* thread = threads[current_thread];
+  if (thread->state == READY)
+  {
+      g_queue_remove(ready_queue, threads[current_thread]);
+  }
+  thread->state = BLOCKED;
+  thread->wake_up_time = uthread_get_total_quantums() + num_quantums;
+
+  if (current_thread == thread->tid)
+  {
+    switch_out_current_thread();
+  }
   return 0;
 }
 
 int uthread_get_tid() {
-  // TODO: Implement this function
-  return 0;
+  return current_thread;
 }
 
 int uthread_get_total_quantums() {
-  // TODO: Implement this function
-  return 0;
+  return TOTAL_QUANTUMS;
 }
 
 int uthread_get_quantums(int tid) {
-  // TODO: Implement this function
-  return 0;
+  if (!is_valid_tid(tid)) return -1;
+
+  return threads[tid]->n_quantum;
 }
+
