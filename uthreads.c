7388ff979509cb6fa6d52de092d66c2ff2787b65
talya <talya.solomon1@mail.huji.ac.il>
2024-05-31 16:11:09 +0300

    3 commit

--- uthreads.c
+++ uthreads.c
@@ -2,10 +2,16 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <glib.h>
+#include <setjmp.h>
 
-#definr READY 0
+#define READY 0
 #define RUNNING 1
 #define BLOCKED 2
+
+typedef unsigned long address_t;
+#define JB_SP 6
+#define JB_PC 7
+
 
 typedef struct Thread {
   int tid;
@@ -16,11 +22,24 @@ typedef struct Thread {
   sigjmp_buf env;
 } Thread;
 
-GQueue *ready_queue = g_queue_new();
-Thread threads[MAX_THREAD_NUM];
+GQueue *ready_queue = NULL;
+Thread* threads[MAX_THREAD_NUM];
 int current_thread = -1;
 
+/* A translation is required when using an address of a variable.
+   Use this as a black box in your code. */
+address_t translate_address(address_t addr)
+{
+  address_t ret;
+  asm volatile("xor    %%fs:0x30,%0\n"
+               "rol    $0x11,%0\n"
+      : "=g" (ret)
+      : "0" (addr));
+  return ret;
+}
+
 int uthread_init(int quantum_usecs) {
+  ready_queue = g_queue_new();
   // TODO: Implement this function
   return 0;
 }
@@ -45,9 +64,11 @@ int uthread_spawn(thread_entry_point entry_point) {
       new_thread->n_quantum = 0;
       new_thread->entry_point = entry_point;
       sigsetjmp(new_thread->env, 1);
-      (new_thread->env->__jmpbuf)[JB_SP] = translate_address
-          (new_thread->stack + STACK_SIZE - sizeof(address_t));
-      (new_thread->env->__jmpbuf)[JB_PC] = translate_address(entry_point);
+      address_t sp = (address_t) new_thread->stack + STACK_SIZE - sizeof
+          (address_t);
+      address_t pc = (address_t) entry_point;
+      (new_thread->env->__jmpbuf)[JB_SP] = translate_address(sp);
+      (new_thread->env->__jmpbuf)[JB_PC] = translate_address(pc);
       sigemptyset(&new_thread->env->__saved_mask);
       threads[i] = new_thread;
       g_queue_push_tail(ready_queue, new_thread);
@@ -55,6 +76,16 @@ int uthread_spawn(thread_entry_point entry_point) {
     }
   }
   return -1;
+}
+
+
+void running_next_thread()
+{
+  Thread* next_thread = g_queue_pop_head(ready_queue);
+  g_queue_push_tail(ready_queue, threads[current_thread]);
+  current_thread = next_thread->tid;
+  next_thread->state = RUNNING;
+  siglongjmp(next_thread->env, 1);
 }
 
 int uthread_terminate(int tid) {
@@ -80,19 +111,6 @@ int uthread_terminate(int tid) {
     free(threads[tid]);
     threads[tid] = NULL;
   return 0;
-}
-
-void running_next_thread() {
-  if (g_queue_is_empty(ready_queue))
-  {
-    printf("thread library error: no more threads to run\n");
-    exit(0);
-  }
-  Thread* next_thread = g_queue_pop_head(ready_queue);
-  next_thread->state = RUNNING;
-  current_thread = next_thread->tid;
-  next_thread->n_quantum++;
-  siglongjmp(next_thread->env, 1);
 }
 
 int uthread_block(int tid) {
